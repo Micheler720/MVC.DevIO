d8f5e70ada4a1feac30fb38f444cb95bc0690971
Michele Ribeiro <micheler720@gmail.com>
2021-03-21 21:40:56 -0300

    Reorganizaçao de projetos

---
+++ src/DevIO.App/Configurations/DependencyInjectionConfig.cs
@@ -0,0 +1,28 @@
+﻿using DevIO.App.Extensions;
+using DevIO.App.Interface;
+using DevIO.App.Notificacoes;
+using DevIO.App.Services;
+using DevIO.Data.Context;
+using DevIO.Data.Repository;
+using Microsoft.AspNetCore.Mvc.DataAnnotations;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace DevIO.App.Configurations
+{
+    public static class DependencyInjectionConfig
+    {
+        public static IServiceCollection ResolveDependency(this IServiceCollection services)
+        {
+            services.AddScoped<MeuDbContext>();
+            services.AddScoped<IProdutoRepository, ProdutoRepository>();
+            services.AddScoped<IFornecedorRepository, FornecedorRepository>();
+            services.AddScoped<IEnderecoRepository, EnderecoRepository>();
+            services.AddSingleton<IValidationAttributeAdapterProvider, MoedaValidationAttributeAdapterProvider>();
+
+            services.AddScoped<INotificador, Notificador>();
+            services.AddScoped<IFornecedorService, FornecedorService>();
+            services.AddScoped<IProdutoService, ProdutoService>();
+            return services;
+        }
+    }
+}
---
+++ src/DevIO.App/Extensions/EmailTagHelper.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Razor.TagHelpers;
+using System.Threading.Tasks;
+
+namespace DevIO.App.Extensions
+{
+    public class EmailTagHelper : TagHelper
+    {
+        public string EmailDomain { get; set; } = "teste.com";
+
+        public async override Task ProcessAsync(TagHelperContext context, TagHelperOutput output)
+        {
+            output.TagName = "a";
+            var content = await output.GetChildContentAsync();
+            var target  = $"{content.GetContent()}@{EmailDomain}";
+            output.Attributes.SetAttribute("href", $"malito:{target}");
+            output.Content.SetContent(target);
+        }
+    }
+}
