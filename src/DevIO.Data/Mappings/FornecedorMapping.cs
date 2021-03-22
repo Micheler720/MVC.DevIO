e48854f0b9d9be3f0435e94eb7749f3c70f2cf3b
Michele Ribeiro <micheler720@gmail.com>
2021-03-21 21:42:16 -0300

    Reorganizaçao de Data

---
+++ src/DevIO.Data/Mappings/FornecedorMapping.cs
@@ -0,0 +1,35 @@
+﻿using DevIO.Business.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace DevIO.Data.Mappings
+{
+    class FornecedorMapping : IEntityTypeConfiguration<Fornecedor>
+    {
+        public void Configure(EntityTypeBuilder<Fornecedor> builder)
+        {
+            builder.HasKey(f => f.Id);
+
+            builder.Property(f => f.Nome)
+                .IsRequired()
+                .HasMaxLength(100);
+
+            builder.Property(f => f.Documento)
+                .IsRequired()
+                .HasColumnType("varchar");
+
+            builder.Property(f => f.Ativo)
+                .IsRequired();
+
+            builder.HasOne(f => f.Endereco)
+                .WithOne(e => e.Fornecedor);
+
+            builder.HasMany(f => f.Produtos)
+                .WithOne(p => p.Fornecedor)
+                .HasForeignKey(p => p.FornecedorId)
+                .OnDelete(DeleteBehavior.ClientSetNull);
+
+            builder.ToTable("Fornecedores");
+        }
+    }
+}
