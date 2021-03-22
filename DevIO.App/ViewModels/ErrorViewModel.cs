3a16ee37260ee80d4b149daae2e36a958867d8d5
Michele Ribeiro <48379411+Micheler720@users.noreply.github.com>
2021-03-21 21:42:45 -0300

    Delete DevIO.App directory

--- DevIO.App/ViewModels/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace DevIO.App.ViewModels
-{
-    public class ErrorViewModel
-    {
-        public string Mensagem { get; set; }
-        public string Titulo { get; set; }
-        public int ErroCode { get; set; }
-    }
-}
